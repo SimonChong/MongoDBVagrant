b9fe0fe416ea4b8fe5197157499784545805c754
Simon Chong <simons.mail@gmail.com>
2014-10-11 10:27:13 +1100

    Updated to use apt to install mongo.

--- puppet/provision.pp
+++ puppet/provision.pp
@@ -2,35 +2,57 @@
 # Install and Run MongoDB Server
 # ===
 
-$mongodbPack = "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.6.5.tgz"
-$splitter0 = split($mongodbPack, '/')
-$mongodbFile = $splitter0[-1]
 
-# Create a directories
-file { "/vagrant/mongo":
-  ensure => "directory",
-  before => Exec['mongo-download'],
+# Import the public key used by the package management system
+exec { "mongo-pk": 
+  command => "sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10",
+  cwd => "/vagrant",   
+  before => Exec['mongo-list-file'],
+  path    => ["/usr/bin", "/usr/sbin", "/bin"]
 }
 
-file { "/data/db":
-  ensure => "directory",
-  before => Exec['mongo-download'],
+# Create a list file for MongoDB.
+exec {"mongo-list-file": 
+  command => "echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list",
+  cwd => "/vagrant",
+  path    => ["/usr/bin", "/usr/sbin", "/bin"],
+  before => Exec['mongo-reload']
 }
 
-# Download Mongo
-exec { "mongo-download": 
-  command => "/usr/bin/wget $mongodbPack",
-  cwd => "/vagrant/mongo",   
-  creates => "/vagrant/mongo/$mongodbFile",
-  before => Package['mongo-extract'],
-  timeout => 0
+# Reload local package database.
+exec {"mongo-reload": 
+  command => "sudo apt-get update",
+  cwd => "/vagrant", 
+  path    => ["/usr/bin", "/usr/sbin", "/bin"],
+  before => Exec['mongo-install']
 }
 
-# Extract Mongo
-exec {"mongo-extract": 
-  command => "tar xvzf $mongodbFile -C /mongodb",
-  cwd => "/vagrant/mongo",   
-  creates => "/mongodb",
+notify {"Installing":
+  message => "Installing MongoDB.. PLEASE WAIT",
+  require => Exec["mongo-reload"],
+  before => Exec['mongo-install']
+}
+
+# Install the MongoDB packages.
+exec {"mongo-install": 
+  command => "sudo apt-get install -y mongodb-org",
+  cwd => "/vagrant", 
+  path    => ["/usr/bin", "/usr/sbin", "/bin"],
+  before => Exec['mongo-start']
+}
+
+# Start MongodDB
+exec {"mongo-start": 
+  command => "sudo service mongod start",
+  cwd => "/vagrant",
+  path    => ["/usr/bin", "/usr/sbin", "/bin"],
+  before => Exec['mongo-check']
+}
+
+# Check MongodDB has started
+exec {"mongo-check": 
+  command => "cat /var/log/mongodb/mongod.log",
+  cwd => "/vagrant",
   path    => ["/usr/bin", "/usr/sbin", "/bin"]
 }
 
